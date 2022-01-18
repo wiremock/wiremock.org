d2083ca978ff7b66531c5584c56cd747888c2657
Neim <elezin9@gmail.com>
2022-01-18 13:46:24 +0100

    footer webinar banner is added

--- vendor/bundle/ruby/2.7.0/gems/ffi-1.11.3/ext/ffi_c/libffi-x86_64-darwin21/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/neimelezi/Desktop/projects/wiremock.org-sources/vendor/bundle/ruby/2.7.0/gems/ffi-1.11.3/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/ruby/2.7.0/gems/rubyzip-2.1.0/lib/zip/version.rb
+++
@@ -1,3 +0,0 @@
-module Zip
-  VERSION = '2.1.0'
-end
