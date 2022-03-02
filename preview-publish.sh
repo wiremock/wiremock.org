c1c9c39000c350010d2b02ae85e408a2b14d2782
Tom Akehurst <t.m.akehurst@gmail.com>
2022-03-02 18:43:15 +0200

    Added step to patch the CNAME file before committing so that it doesn't try to set it to wiremock.org

--- preview-publish.sh
+++ preview-publish.sh
@@ -6,6 +6,7 @@ bundle exec jekyll build
 cp -rf _site/* ../wiremock.org-preview/
 
 pushd ../wiremock.org-preview 
+echo 'private-preview.wiremock.org' > CNAME
 git add --all
 git commit -m "Updated docs"
 git push origin gh-pages
