d73ee75f77e4ac3fbe79fa89a1ef469c68a2081e
Mark Gerrard <gerrardmark@gmail.com>
2022-11-03 17:50:51 +0000

    add faq sections

---
+++ assets/js/faq.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.faq-list > dd').forEach(function (dd) {
+    dd.style.display = 'none';
+});
+
+document.querySelector('.faq-list').addEventListener('click', function (event) {
+    if (event.target.tagName === 'DT') {
+        var definition = event.target.nextElementSibling;
+        definition.style.display = definition.style.display === 'block' ? 'none' : 'block';
+    }
+});
