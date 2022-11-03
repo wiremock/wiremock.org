478ec317f7430456afff1517f6f5edb74ab9f404
Mark Gerrard <gerrardmark@gmail.com>
2022-11-03 17:59:10 +0000

    only run faq js when faq list exists

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,10 +1,14 @@
-document.querySelectorAll('.faq-list > dd').forEach(function (dd) {
-    dd.style.display = 'none';
-});
+var faqList = document.querySelector('.faq-list');
 
-document.querySelector('.faq-list').addEventListener('click', function (event) {
-    if (event.target.tagName === 'DT') {
-        var definition = event.target.nextElementSibling;
-        definition.style.display = definition.style.display === 'block' ? 'none' : 'block';
-    }
-});
+if (faqList) {
+    document.querySelectorAll('.faq-list > dd').forEach(function (dd) {
+        dd.style.display = 'none';
+    });
+
+    document.querySelector('.faq-list').addEventListener('click', function (event) {
+        if (event.target.tagName === 'DT') {
+            var definition = event.target.nextElementSibling;
+            definition.style.display = definition.style.display === 'block' ? 'none' : 'block';
+        }
+    });
+}
