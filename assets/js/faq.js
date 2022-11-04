756689e074a8b9fecb9b58990d13c63dd21f88bc
Mark Gerrard <l.imptwiglet@gmail.com>
2022-11-04 15:35:00 +0000

    improve styling of faq, add arrow toggle

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,14 +1,21 @@
 var faqList = document.querySelector('.faq-list');
 
 if (faqList) {
-    document.querySelectorAll('.faq-list > dd').forEach(function (dd) {
-        dd.style.display = 'none';
+    document.querySelectorAll('.faq-list > dt').forEach(function (dd) {
+        dd.classList.add('faq-list--collapsed');
     });
 
     document.querySelector('.faq-list').addEventListener('click', function (event) {
         if (event.target.tagName === 'DT') {
-            var definition = event.target.nextElementSibling;
-            definition.style.display = definition.style.display === 'block' ? 'none' : 'block';
+            var dd = event.target;
+
+            if (dd.classList.contains('faq-list--expanded')) {
+                dd.classList.remove('faq-list--expanded');
+                dd.classList.add('faq-list--collapsed');
+            } else {
+                dd.classList.add('faq-list--expanded');
+                dd.classList.remove('faq-list--collapsed');
+            }
         }
     });
 }
