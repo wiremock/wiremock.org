ef1bacc23d385803f09a898522ddfecf0f686b9f
Oleg Nenashev <o.v.nenashev@gmail.com>
2023-05-05 12:23:19 +0200

    Cleanup WireMock Cloud references in code and metadata

--- assets/js/tracking-events.js
+++ assets/js/tracking-events.js
@@ -7,6 +7,5 @@ function addGaEventListener(elementId, location, action, description) {
     }
 }
 
-addGaEventListener('wiremock-studio-download', 'studio_getting_started', 'download_studio_jar_clicked', 'Download WireMock Studio JAR clicked');
 addGaEventListener('wiremock-standalone-download', 'oss_docs', 'download_standalone_jar_clicked', 'Download WireMock Standalone JAR clicked');
 addGaEventListener('slack-link', 'studio_gettnavigationing_started', 'slack_link_clicked', 'Slack link clicked');
