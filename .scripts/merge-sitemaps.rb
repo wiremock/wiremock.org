11bcd69ba5ada6275a2dbd612d2e1a2504571148
Oleg Nenashev <o.v.nenashev@gmail.com>
2023-12-10 18:22:31 +0100

    Include documentation sitemap into the bundle

--- .scripts/merge-sitemaps.rb
+++ .scripts/merge-sitemaps.rb
@@ -1,5 +1,5 @@
 # Merges sitemaps, to be called from the root directory
-header, footer, content_main, content_2x, content_3x = []
+header, footer, content_main, content_2x, content_docs = []
 
 File.open( "_site/sitemap.xml", 'r' ) do |f1|
     content_main = (IO.readlines f1)
@@ -13,16 +13,14 @@ File.open( ".submodules/wiremock.org-2.x/tmp/site_2x/sitemap.xml", 'r' ) do |f2|
     content_2x.slice!( -1 ) 
 end
 
-#File.open( "./tmp/site_3x/sitemap.xml", 'r' ) do |f3|
-#    content_3x = (IO.readlines f3)
-#    content_3x.slice!( 0..11 )
-#    content_3x.slice!( -1 ) 
-#end
-
-# TODO: Add MkDocs sitemap
+File.open( "./_site-docs/sitemap.xml", 'r' ) do |f3|
+    content_docs = (IO.readlines f3)
+    content_docs.slice!( 0..11 )
+    content_docs.slice!( -1 ) 
+end
 
 File.open( "_site/sitemap.xml", 'w' ) do |f4|
-    f4.write ( header + content_main + content_2x + footer ).join()
+    f4.write ( header + content_main + content_docs + content_2x + footer ).join()
 end
 
 
