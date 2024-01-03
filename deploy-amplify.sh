bf32730443dd0f9cee9ef59e50d58e83b9f20bd3
Tom Akehurst <tom@wiremock.org>
2024-01-03 21:06:14 +0000

    Quietened down the workflow a bit

--- deploy-amplify.sh
+++ deploy-amplify.sh
@@ -3,12 +3,12 @@
 set -eo pipefail
 
 rm -f wiremock-org-amplify.zip
-zip -r wiremock-org-amplify.zip _site/*
+zip -q -r wiremock-org-amplify.zip _site/*
 
 deploymentData=$(aws amplify create-deployment --app-id d1rkup1qsf4ckf --branch-name prod)
 jobId=$(echo $deploymentData | jq -r '.jobId')
 uploadUrl=$(echo $deploymentData | jq -r '.zipUploadUrl')
 
-curl -v -X PUT -H 'content-type:application/zip' -T wiremock-org-amplify.zip "$uploadUrl"
+curl -X PUT -H 'content-type:application/zip' -T wiremock-org-amplify.zip "$uploadUrl"
 
 aws amplify start-deployment --app-id d1rkup1qsf4ckf --job-id $jobId --branch-name prod
