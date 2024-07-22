d966ba224327759b2a793f49cf5687ba33f87139
Ethan Jones <ethan@Ethans-MacBook-Pro.local>
2024-07-22 14:10:17 -0400

    Updating WM Live callout

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -5,8 +5,8 @@ var notifications = [
   {
   content: {
     title:
-    "Go beyond the basics of request matching with WireMock Live on July 18! " +
-    "<a href='https://info.wiremock.io/wiremock-live-demos-1-0?utm_source=oss-wmlive&utm_medium=oss-wmlive&utm_campaign=oss-wmlive&utm_id=oss-wmlive&utm_term=oss-wmlive' target='_blank'>Register now.</a>",
+    "Learn the basics of our new stateful mocking at WireMock Live on August 22" +
+    "<a href='https://info.wiremock.io/wiremock-live-demos-24-08?utm_source=oss-wmlive&utm_medium=oss-wmlive&utm_campaign=oss-wmlive&utm_id=oss-wmlive&utm_term=oss-wmlive' target='_blank'>Register now.</a>",
   },
   layout: {
     style: "wiremock_notification_with_link",
