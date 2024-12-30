eee314253564dfad4147966f931126c00ba79d41
leeturner <lee@leeturner.me>
2024-12-30 09:23:53 +0000

    Remove the old banner notification as it references an old event

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,21 +1,7 @@
 var wiremock_notification_shown = localStorage.getItem(
   "wiremock_notification_shown",
 );
-var notifications = [
-  {
-  content: {
-    title:
-    "Learn the basics of our new stateful mocking at WireMock Live on August 22" +
-    "<a href='https://info.wiremock.io/wiremock-live-demos-24-08?utm_source=oss-wmlive&utm_medium=oss-wmlive&utm_campaign=oss-wmlive&utm_id=oss-wmlive&utm_term=oss-wmlive' target='_blank'>Register now.</a>",
-  },
-  layout: {
-    style: "wiremock_notification_with_link",
-    position: "top",
-    className: "info",
-    autoHide: false,
-  },
-  },
-];
+var notifications = [];
 
 if (wiremock_notification_shown == null) {
   localStorage.setItem("wiremock_notification_shown", true);
